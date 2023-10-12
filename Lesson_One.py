6ab17533d218e6fd3142811e8946cc3ecb7066bd
Development <chris.dev@apollo.localdomain>
2023-10-12 10:36:47 -0700

    added more examples

--- Lesson_One.py
+++ Lesson_One.py
@@ -1,1 +1,19 @@
 print("Hello")
+answer1 = input("What is your name: ")
+print(f"Your name is {answer1}")
+
+answer2 = input("What is the name of your best friend? ")
+print(f"Your best friend's name is {answer2}")
+
+answer3 = input("Where do you live? ")
+print(f"You live in {answer3}")
+
+
+age = input("What is your age? ")
+print(type(age))
+
+intage = int(age)
+floatage = float(age)
+
+print(intage)
+print(floatage)
